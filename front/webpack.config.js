85a35850d0f976b1f5082a8cc4dd16691d569755
moisesVeSouza <mvs338409@hotmail.com>
2025-12-15 23:18:04 -0300

    initial commit

---
+++ front/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    },
+    port: 3000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
